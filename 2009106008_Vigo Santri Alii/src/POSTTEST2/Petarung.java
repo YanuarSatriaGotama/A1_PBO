2776743b9f3931a4e2e3b834e72629f9249732a0
Eviq35 <vigosantriali@gmail.com>
2022-04-05 21:35:15 +0800

    POSTTEST 2 008

---
+++ 2009106008_Vigo Santri Alii/src/POSTTEST2/Petarung.java
@@ -0,0 +1,16 @@
+package POSTTEST2;
+
+public class Petarung {
+    String nama;
+    long id;
+    String bela_diri;
+    String kelas;
+    
+    public Petarung( long id, String nama, String bela_diri, String kelas){
+        this.nama = nama;
+        this.id = id;
+        this.bela_diri = bela_diri;
+        this.kelas = kelas;
+    }
+    
+}
