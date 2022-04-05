6964dbcb4d3443c1d52b1f5444ab0af24c8d42bc
YanuarSatriaGotama <103011475+YanuarSatriaGotama@users.noreply.github.com>
2022-04-05 10:25:23 +0800

    2009106013_Yanuar Satria Gotama_POSTTEST2
    2009106013_Yanuar Satria Gotama_POSTTEST2

---
+++ 2009106013_Yanuar Satria Gotama_PostTest2/src/Perusahaan/Admin.java
@@ -0,0 +1,11 @@
+package Perusahaan;
+public class Admin {
+    String namaAdmin;
+    
+    public Admin(String namaAdmin){
+        this.namaAdmin = namaAdmin;
+    }
+    void sebutAdmin(){
+        System.out.println("Saya adalah Admin");
+    }
+}
