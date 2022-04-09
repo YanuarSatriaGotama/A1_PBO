c5ef67e124a9fa124192ed68a572b25ce5c439a4
YanuarSatriaGotama <103011475+YanuarSatriaGotama@users.noreply.github.com>
2022-04-09 19:33:39 +0800

    2009106013_Yanuar Satria Gotama_POSTTEST3
    2009106013_Yanuar Satria Gotama_POSTTEST3

---
+++ 2009106013_Yanuar Satria Gotama_PostTest3/src/Perusahaan/Admin.java
@@ -0,0 +1,26 @@
+package Perusahaan;
+public class Admin {
+    private String Nama;
+    private int Id_admin;
+    private boolean Utama;
+    
+    public void setNama(String Nama){
+        this.Nama = Nama;
+    }
+    public void setId_admin(int Id_admin){
+        this.Id_admin = Id_admin;
+    }
+    public void setUtama(boolean Utama){
+        this.Utama = Utama;
+    }
+    
+    public String getNama(){
+        return Nama;
+    }
+    public int getId_admin(){
+        return Id_admin;
+    }
+    public boolean getUtama(){
+        return Utama;
+    }
+}
