b0d5ab884644396a29320944fbf875b6597ad376
YanuarSatriaGotama <103011475+YanuarSatriaGotama@users.noreply.github.com>
2022-04-29 19:20:13 +0800

    2009106013_YanuarSatriaGotama_POSTTEST
    2009106013_YanuarSatriaGotama_POSTTEST

---
+++ 2009106013_YanuarSatriaGotama_POSTTEST4/src/Perusahaan/ManajerGudang.java
@@ -0,0 +1,18 @@
+package Perusahaan;
+public class ManajerGudang extends Manajer {
+    public ManajerGudang(String nama, int id, String noTelp, String email) {
+        super(nama, id, noTelp, email);
+    }
+    
+    static void pantau(){
+        System.out.println("Manajer memantau Gudang");
+    }
+    
+    static void lapor(String tanggal){
+        System.out.println("Manajer membuat laporan barang gudang pada tanggal " +tanggal);
+    }
+
+    static void lapor(String tanggal, String barang){
+        System.out.println("Manajer sedang membuat laporan cek " +barang+ " pada tanggal " +tanggal);
+    }
+}
